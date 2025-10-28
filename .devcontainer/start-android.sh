f9c89a68a5b27ec808447e02000ebc0678d6eb65
Kronos <152859792+Samuel-Belay@users.noreply.github.com>
2025-10-28 19:29:07 +0000

    Modify VNC server configuration in start-android.sh
    Updated VNC server startup options to disable password requirement.

--- .devcontainer/start-android.sh
+++ .devcontainer/start-android.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-# ---------------------- Start VNC server ----------------------
+
+# ---------------------- Start Xvfb ----------------------
 mkdir -p ~/.vnc
 Xvfb :0 -screen 0 1280x720x24 &
 export DISPLAY=:0
 
-x11vnc -forever -usepw -display :0 -shared &
+# ---------------------- Start VNC server ----------------------
+x11vnc -display :0 -forever -shared -nopw &
 
 # ---------------------- Start Android emulator ----------------------
 $ANDROID_SDK_ROOT/emulator/emulator -avd pixel6 -gpu host -no-snapshot-load &
