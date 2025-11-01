f941a7f96612df9f338a334443196017be3a1ce5
Kronos <152859792+Samuel-Belay@users.noreply.github.com>
2025-11-01 21:19:44 +0000

    Modify download-sdk.sh to always download SDK tools
    Updated script to always download and extract the SDK tools.

--- scripts/download-sdk.sh
+++ scripts/download-sdk.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-SDK_DIR=$ANDROID_SDK_ROOT/cmdline-tools/latest
+mkdir -p $ANDROID_SDK_ROOT/cmdline-tools/latest
 
-# Only download if not already present
-if [ ! -f "$SDK_DIR/bin/sdkmanager" ]; then
-    wget https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip -O /tmp/cmdline-tools.zip
-    unzip /tmp/cmdline-tools.zip -d $SDK_DIR
-    rm /tmp/cmdline-tools.zip
-fi
+cd /tmp
+wget https://dl.google.com/android/repository/commandlinetools-linux-9477386_latest.zip -O cmdline-tools.zip
+unzip cmdline-tools.zip -d $ANDROID_SDK_ROOT/cmdline-tools/latest
+rm cmdline-tools.zip
